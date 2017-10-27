8db7aa0e5e912ba71a50d7fe657fbf169b63bde6
tudoistube <tudoistube@ymail.com>
2017-10-27 15:51:37 +0900

    empty repo

--- zblog/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ZblogConfig(AppConfig):
-    name = 'zblog'
--- zvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\joywins\djangogirls\zvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
