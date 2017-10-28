fd5805b4710a10f959a46ef4481498f9cbab72f3
tudoistube <tudoistube@ymail.com>
2017-10-28 11:34:21 +0900

    model and template

--- zblog/views.py
+++ zblog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+#...from 다음에 있는 마침표(.)는 현재 디렉토리 또는 애플리케이션을 의미합니다.
+from .models import Zpost
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
     #return render(request, 'post_list.html')
-    return render(request, 'zblog/post_list.html', {})
+    #return render(request, 'zblog/post_list.html', {})
+    posts = Zpost.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'zblog/post_list.html', {'posts': posts})
