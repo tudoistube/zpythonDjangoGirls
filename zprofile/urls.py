39a6c4855138aceff8acaff40a0d93de3b188b14
tudoistube <tudoistube@ymail.com>
2018-05-10 22:42:34 +0900

    add home_en.html

--- zprofile/urls.py
+++ zprofile/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.profile_list, name='profile_list'),
+    url(r'^$', views.home, name='home'),
+    url(r'^en/$', views.home_en, name='home_en'),
 ]
--- zprofile/views.py
+++ zprofile/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-def profile_list(request):
+def home(request):
     #return render(request, 'post_list.html')
     #return render(request, 'zblog/post_list.html', {})
     #posts = Zpost.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'zprofile/profile_list.html', {})
+    return render(request, 'zprofile/home.html', {})
+
+
+def home_en(request):
+    #pass
+    # request.POST, request.FILES
+    return render(request, 'zprofile/home_en.html', {})
