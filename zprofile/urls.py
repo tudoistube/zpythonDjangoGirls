9c2a6ab7d16994ca0edbc82c33ea9e9082e93a6c
hanmydog <hanmydog@chol.com>
2018-05-11 16:03:46 +0900

    arrange irrelevant urls

--- zprofile/urls.py
+++ zprofile/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^en/$', views.home_en, name='home_en'),
-    url(r'^hmd/$', views.home_hmd, name='home_hmd'),
+    #url(r'^hmd/$', views.home_hmd, name='home_hmd'),
 ]
